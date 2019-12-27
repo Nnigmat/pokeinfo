c598b0b08ec66a87dfc9941ca15697197717af89
Nikita <nnigmat@gmail.com>
2019-12-27 13:07:29 +0300

    Updates

---
+++ precache-manifest.402b66d6787a93eb48eba1d42d5a241a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54cc16d8134e3e7d0d0ede5246b3893",
+    "url": "/pokeinfo/index.html"
+  },
+  {
+    "revision": "974a02cfb938a67c21a5",
+    "url": "/pokeinfo/static/js/2.5ed23bfc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pokeinfo/static/js/2.5ed23bfc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d84258869405699868f5",
+    "url": "/pokeinfo/static/js/main.071e9418.chunk.js"
+  },
+  {
+    "revision": "d6028f47d154f5af55eb",
+    "url": "/pokeinfo/static/js/runtime-main.2b355641.js"
+  }
+]);
